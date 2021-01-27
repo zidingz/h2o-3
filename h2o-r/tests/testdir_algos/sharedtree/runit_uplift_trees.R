e9687b6665481f95c9986f90550a9c5ada11654b
maurever <veronika@maurerova.cz>
2021-01-27 17:09:15 +0100

    PUBDEV-4940 fix uplift column in score dataset

--- h2o-r/tests/testdir_algos/sharedtree/runit_uplift_trees.R
+++ h2o-r/tests/testdir_algos/sharedtree/runit_uplift_trees.R
@@ -11,8 +11,10 @@ test.uplift <- function() {
     ### simulate data for uplift modeling
 
     set.seed(123)
-    train <- sim_pte(n = 100, p = 6, rho = 0, sigma = sqrt(2), beta.den = 4)
+    train <- sim_pte(n = 1000, p = 6, rho = 0, sigma = sqrt(2), beta.den = 4)
     train$treat <- ifelse(train$treat == 1, 1, 0)
+    
+    ntrees <- 1000
     
     print("Train data summary")
     print(summary(train))
@@ -23,7 +25,7 @@ test.uplift <- function() {
         data = train,
         split_method = "KL",
         mtry = 6,
-        ntree = 1000,
+        ntree = ntrees,
         #interaction.depth = 10,
         minsplit = 10,
         min_bucket_ct0 = 10,
@@ -37,13 +39,8 @@ test.uplift <- function() {
     print("Uplift predict on test data")
     test <- sim_pte(n = 200, p = 20, rho = 0, sigma =  sqrt(2), beta.den = 4)
     test$treat <- ifelse(test$treat == 1, 1, 0)
-    predUplift <- predict(modelUplift, test)
+    predUplift <- predict(modelUplift, train)
     print(head(predUplift))
-
-    #print("Uplift performance")
-    #perf <- performance(predUplift[, 1], predUplift[, 2], test$y, test$treat, direction = 1)
-    #print(perf)
-    #plot(perf[, 8] ~ perf[, 1], type ="l", xlab = "Decile", ylab = "uplift")
 
     # fit h2o RF
     train$treat <- as.factor(train$treat)
@@ -54,16 +51,29 @@ test.uplift <- function() {
         uplift_column = "treat",
         uplift_metric = "KL",
         distribution = "bernoulli",
-        ntrees = 1000,
+        gainslift_bins = 10,
+        ntrees = ntrees,
         max_depth = 10,
         min_rows = 10,
         nbins = 100,
         seed = 42)
 
+    print(h2o.gainsLift(modelH2o))
+    print(h2o.varimp(modelH2o))
+    
     # predict upliftRF on new data for treatment group
-    testH2oTreat <- as.h2o(test)
+    testH2oTreat <- as.h2o(train)
     predH2oTreat <- predict(modelH2o, testH2oTreat)
     print(head(predH2oTreat))
+
+    res <- as.data.frame(predH2oTreat)
+    res$pr.y1_ct1 <- predUplift[,1]
+    res$pr.y1_ct0 <- predUplift[,2]
+    res$div_ct1 <- res$p0 - res$pr.y1_ct1
+    res$div_ct0 <- res$p1 - res$pr.y1_ct0
+    print(head(res))
+    print(summary(res))
+
     
 }
 
